b9b844e2bf93f972a2108d73fcc334aafdcd483b
Cristian Tone <nelu-cristian.tone@adesso-romania.ro>
2026-07-16 17:55:08 +0300

    Rename comment

--- src/main.jsx
+++ src/main.jsx
@@ -17,7 +17,7 @@ const queryClient = new QueryClient({
       staleTime: 10_000, //how much time data live in cache
       refetchOnReconnect: true, //refetch stale data when the internet connection is up again
       refetchOnWindowFocus: false, //refetch stale data when the user switch back to browser tab of the app
-      refetchInterval: 60_000, // fetch automatically data every 5 sec
+      refetchInterval: 60_000, // fetch automatically data every 60 sec
       placeholderData: keepPreviousData, // keep previous data as placeholder before retrieving new data from server
     },
   },
